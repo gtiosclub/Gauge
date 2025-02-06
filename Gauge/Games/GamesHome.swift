afc2cf56b576250424a3ac9bfc9998dccdd309b8
Akshat-Shenoi <ashenoi3@gatech.edu>
2025-02-06 18:07:16 -0500

    Added Games Folder & Files

---
+++ Gauge/Games/GamesHome.swift
@@ -0,0 +1,19 @@
+//
+//  GamesHome.swift
+//  Gauge
+//
+//  Created by Akshat Shenoi on 2/6/25.
+//
+
+import SwiftUI
+
+struct GamesHome: View {
+    // Will include a tab list view of all the games in the app
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GamesHome()
+}
