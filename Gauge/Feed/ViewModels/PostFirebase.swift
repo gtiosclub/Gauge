009de7d65c29e206ef821b7499a694470863782f
Krish Prasad <krishkp00@gmail.com>
2025-02-07 14:07:33 -0500

    add createSliderPost

--- Gauge/Feed/ViewModels/PostFirebase.swift
+++ Gauge/Feed/ViewModels/PostFirebase.swift
@@ -10,8 +10,7 @@ import Foundation
 class PostFirebase {
     
     func createBinaryPost(userId: String, category: Category, question: String, responseOption1: String, responseOption2: String) {
-        
-        let documentRef = Firebase.db.collection("users").addDocument(data: [:])
+        let documentRef = Firebase.db.collection("POSTS").addDocument(data: [:])
 
         documentRef.setData([
             "type": PostType.BinaryPost.rawValue,
@@ -26,6 +25,35 @@ class PostFirebase {
             "responseOption2": responseOption2,
             "responseResult1": 0,
             "responseResult2": 0
+        ]) { error in
+            if let error = error {
+                print("error writing doc: \(error)")
+            } else {
+                print("added new post to POSTS")
+                documentRef.collection("COMMENTS")
+                documentRef.collection("RESPONSES")
+                
+            }
+        }
+    }
+    
+    func createSliderPost(userId: String, category: Category, question: String, lowerBoundValue: Double, upperBoundValue: Double, lowerBoundLabel: String, upperBoundLabel: String) {
+        let documentRef = Firebase.db.collection("POSTS").addDocument(data: [:])
+
+        documentRef.setData([
+            "type": PostType.SliderPost.rawValue,
+            "postId": UUID().uuidString,
+            "userId": userId,
+            "category": category.rawValue,
+            "viewCounter": 0,
+            "responseCounter": 0,
+            "postDateAndTime": Date(),
+            "question": question,
+            "lowerBoundValue": lowerBoundValue,
+            "upperBoundValue": upperBoundValue,
+            "lowerBoundLabel": lowerBoundLabel,
+            "upperBoundLabel": upperBoundLabel,
+            "responseResults": []
         ]) { error in
             if let error = error {
                 print("error writing doc: \(error)")
