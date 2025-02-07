b3a0a11e35993a2e5569dae07583985ac93903d4
Krish Prasad <krishkp00@gmail.com>
2025-02-07 13:50:00 -0500

    add createBinaryPost

--- Gauge/Feed/ViewModels/PostFirebase.swift
+++ Gauge/Feed/ViewModels/PostFirebase.swift
@@ -9,4 +9,33 @@ import Foundation
 
 class PostFirebase {
     
+    func createBinaryPost(userId: String, category: Category, question: String, responseOption1: String, responseOption2: String) {
+        
+        let documentRef = Firebase.db.collection("users").addDocument(data: [:])
+
+        documentRef.setData([
+            "type": PostType.BinaryPost.rawValue,
+            "postId": UUID().uuidString,
+            "userId": userId,
+            "category": category.rawValue,
+            "viewCounter": 0,
+            "responseCounter": 0,
+            "postDateAndTime": Date(),
+            "question": question,
+            "responseOption1": responseOption1,
+            "responseOption2": responseOption2,
+            "responseResult1": 0,
+            "responseResult2": 0
+        ]) { error in
+            if let error = error {
+                print("error writing doc: \(error)")
+            } else {
+                print("added new post to POSTS")
+                documentRef.collection("COMMENTS")
+                documentRef.collection("RESPONSES")
+                
+            }
+        }
+    }
+    
 }
