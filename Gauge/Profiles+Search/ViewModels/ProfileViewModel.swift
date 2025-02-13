bbeee062f2630327145bca4e8aa396676fb64bd9
Amber Verma <107515154+ambervermaa@users.noreply.github.com>
2025-02-13 17:30:19 -0500

    Created ProfileViewModel

---
+++ Gauge/Profiles+Search/ViewModels/ProfileViewModel.swift
@@ -0,0 +1,21 @@
+//
+//  ProfileViewModel.swift
+//  Gauge
+//
+//  Created by amber verma on 2/13/25.
+//
+
+import Foundation
+import FirebaseFirestore
+
+class ProfileViewModel: ObservableObject {
+    @Published var user: User?
+    
+    init() {
+        
+    }
+    
+    
+    // implement your funcs here:
+    
+}
