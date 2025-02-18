c7d569b5ecd96403a4bd98c59ac4f55fb864e608
sam-orouji <samorouji5267@gmail.com>
2025-02-18 17:56:07 -0500

    created update and remove profile picture functions for firestore

--- Gauge/Profiles+Search/ViewModels/ProfileViewModel.swift
+++ Gauge/Profiles+Search/ViewModels/ProfileViewModel.swift
@@ -16,6 +16,38 @@ class ProfileViewModel: ObservableObject {
     }
     
     
-    // implement your funcs here:
+    //add/update profile picture
+    func updateProfilePicture(userID: String, image: UIImage) async -> Void {
+        //update profilePhoto field
+        guard let profilePhoto = storeImageAndReturnURL(image: image) else {
+            print("Failed to get download URL")
+            return
+        }
+        //update profile picture in firestore
+        let userDocument = Firebase.db.collection("USERS").document(userID)
+        userDocument.updateData(["profilePhoto": profilePhoto]) { error in
+            if let error = error {
+                print("Error updating document: \(error.localizedDescription)")
+            } else {
+                print("Document successfully updated")
+            }
+        }
+        
+    }
+    
+    
+    
+    //remove profile picture
+    func removeProfilePicture(userID: String) async -> Void {
+        
+        let userDocument = Firebase.db.collection("USERS").document(userID)
+        
+        userDocument.updateData(["profilePhoto": FieldValue.delete()]) { error in
+            if let error = error {
+                print("Error updating document: \(error.localizedDescription)")
+            }
+        }
+    }
+    
     
 }
