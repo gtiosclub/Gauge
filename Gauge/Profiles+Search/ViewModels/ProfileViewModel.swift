acef9e977414fb426ee5f04bf1786d7d22cd19b6
sam-orouji <samorouji5267@gmail.com>
2025-02-20 17:35:31 -0500

    getProfilePic method created, fixed removeProfilePicture

--- Gauge/Profiles+Search/ViewModels/ProfileViewModel.swift
+++ Gauge/Profiles+Search/ViewModels/ProfileViewModel.swift
@@ -16,6 +16,8 @@ class ProfileViewModel: ObservableObject {
     }
     
     
+    
+    //store the image URL that is provided by FirebaseStorage
     //add/update profile picture
     func updateProfilePicture(userID: String, image: UIImage) async -> Void {
         //update profilePhoto field
@@ -36,16 +38,45 @@ class ProfileViewModel: ObservableObject {
     }
     
     
-    
     //remove profile picture
     func removeProfilePicture(userID: String) async -> Void {
         
+        //update profile picture in firestore
         let userDocument = Firebase.db.collection("USERS").document(userID)
         
+        //delete data in database
         userDocument.updateData(["profilePhoto": FieldValue.delete()]) { error in
             if let error = error {
                 print("Error updating document: \(error.localizedDescription)")
+            } else {
+                print("Document successfully updated")
             }
+        }
+        
+        //removeAndReturn returns true if image successfully removed from firebaseStorage
+        guard let removed = removeAndReturn(userID) else {
+            print("failed to successfully remove profile picture")
+        }
+
+    }
+    
+    
+    
+    //get pfp function
+    func getProfilePicture(userID: String) async -> Void {
+        //user
+        let userDocument = Firebase.db.collection("USERS").document(userID)
+        
+        do {
+            let document = try await userDocument.getDocument()
+            if let data = document.data(),
+               let urlString = data["profilePicture"] as? String {
+                return await getImageFromURL(urlString: urlString)
+            } else {
+                print("Document does not exist or URL could not be retrieved")
+            }
+        } catch {
+            print("Error fetching document: \(error.localizedDescription)")
         }
     }
     
