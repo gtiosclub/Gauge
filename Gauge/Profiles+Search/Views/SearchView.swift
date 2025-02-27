17d4b4f12f82a791883ab1c218800c307892060b
anthonyle51 <anthonyle20045@gmail.com>
2025-02-27 16:40:53 -0500

    search bar

--- Gauge/Profiles+Search/Views/SearchView.swift
+++ Gauge/Profiles+Search/Views/SearchView.swift
@@ -8,8 +8,40 @@
 import SwiftUI
 
 struct SearchView: View {
+    @State private var searchText: String = ""
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            VStack {
+                // Search Bar
+                HStack {
+                    HStack {
+                        Image(systemName: "magnifyingglass")
+                            .foregroundColor(.gray)
+                        
+                        TextField("Search", text: $searchText)
+                            .foregroundColor(.gray)
+                    }
+                    .padding(.horizontal)
+                    .padding(.vertical, 8)
+                    .background(Color(.systemGray6))
+                    .cornerRadius(12)
+                }
+                .padding(.horizontal)
+                .padding(.bottom)
+                .background(Color(red: 0.4627, green: 0.8392, blue: 1.0))
+                Spacer()
+                
+                
+                
+                
+                
+                
+                
+                
+                
+            }
+            .navigationTitle("Explore")
+        }
     }
 }
 
