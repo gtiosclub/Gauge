78c0f4f3d421cb98191cb152030f4cc546359936
HawthorneB <hawthorne.f.brown@gmail.com>
2025-03-02 16:11:13 -0500

    Implements the view for the binary post

---
+++ Gauge/Feed/Views/BinaryFeedPost.swift
@@ -0,0 +1,18 @@
+//
+//  BinaryFeedPost.swift
+//  Gauge
+//
+//  Created by HB on 2/27/25.
+//
+
+import SwiftUI
+
+struct BinaryFeedPost: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BinaryFeedPost()
+}
