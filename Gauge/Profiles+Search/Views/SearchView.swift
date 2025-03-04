e1f46815b0d29aac736b792f2ffe3517b0580f1a
anthonyle51 <anthonyle20045@gmail.com>
2025-03-04 14:52:10 -0500

    search

--- Gauge/Profiles+Search/Views/SearchView.swift
+++ Gauge/Profiles+Search/Views/SearchView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct SearchView: View {
     @State private var searchText: String = ""
+    @FocusState private var isSearchFieldFocused: Bool
     let items = Array(1...50).map { "Category \($0)" }
     
     // Define the grid columns
@@ -16,73 +17,120 @@ struct SearchView: View {
         GridItem(.flexible()),
         GridItem(.flexible())
     ]
+    
     var body: some View {
-        NavigationView {
+        NavigationStack {
             VStack {
                 // Search Bar
                 HStack {
                     HStack {
                         Image(systemName: "magnifyingglass")
-                            .foregroundColor(.gray)
+                            .foregroundColor(Color(.systemGray))
                         
                         TextField("Search", text: $searchText)
-                            .foregroundColor(.gray)
+                            .foregroundColor(Color(.black))
+                            .focused($isSearchFieldFocused)
+                            .onTapGesture {
+                                isSearchFieldFocused = true
+                            }
+                            .onAppear {
+                                DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                                    isSearchFieldFocused = true
+                                }
+                            }
                     }
                     .padding(.horizontal)
-                    .padding(.vertical, 8)
-                    .background(Color(.systemGray6))
+                    .padding(.vertical, 5)
+                    .background(Color(.systemGray5))
                     .cornerRadius(12)
+                    
+                    if isSearchFieldFocused {
+                        Button("Cancel") {
+                            isSearchFieldFocused = false
+                            searchText = ""
+                        }
+                        .foregroundColor(.blue)
+                        .padding(.leading, 1)
+                        .transition(.move(edge: .trailing))
+                        .animation(.easeInOut, value: isSearchFieldFocused)
+                    }
                 }
                 .padding(.horizontal)
-                .padding(.bottom, 20)
-                .background(Color(red: 0.4627, green: 0.8392, blue: 1.0))
+                .padding(.top, 1)
+                .padding(.bottom, 10)
                 
-                // Categories
-                Text("Categories")
-                    .font(.headline)
-                    .bold()
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                    .padding(.horizontal)
-                
-                ScrollView {
-                    LazyVGrid(columns: columns, spacing: 10) {
-                        ForEach(items.indices, id: \.self) { index in
-                            if index < 2 {
-                                Section {
-                                    
-                                } header: {
+                if isSearchFieldFocused {
+                    RecentSearchesView(isSearchFieldFocused: $isSearchFieldFocused, searchText: $searchText)
+                } else {
+                    // Categories
+                    Text("Categories")
+                        .font(.headline)
+                        .bold()
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .padding(.horizontal)
+                    
+                    ScrollView {
+                        LazyVGrid(columns: columns, spacing: 10) {
+                            ForEach(items.indices, id: \.self) { index in
+                                if index < 2 {
+                                    // Large Categories
+                                    Section {
+                                        
+                                    } header: {
+                                        ZStack {
+                                            RoundedRectangle(cornerRadius: 10)
+                                                .fill(Color(.systemGray5))
+                                                .frame(height: 100)
+                                            Text(items[index])
+                                                .foregroundColor(Color(.black))
+                                                .font(.headline)
+                                        }
+                                    }
+                                } else {
+                                    // Small Categories
                                     ZStack {
                                         RoundedRectangle(cornerRadius: 10)
-                                            .fill(Color.red)
+                                            .fill(Color(.systemGray5))
                                             .frame(height: 100)
+                                        
                                         Text(items[index])
-                                            .foregroundColor(.white)
+                                            .foregroundColor(.black)
                                             .font(.headline)
                                     }
-                                }
-                            } else {
-                                ZStack {
-                                    RoundedRectangle(cornerRadius: 10)
-                                        .fill(Color.blue)
-                                        .frame(height: 100)
-                                    
-                                    Text(items[index])
-                                        .foregroundColor(.white)
-                                        .font(.headline)
                                 }
                             }
                         }
                     }
+                    .padding(.horizontal)
                 }
-                .padding(.horizontal)
-                
-                
-                
-                
-                
-                
             }
-            .navigationTitle("Explore")
+            .navigationTitle(isSearchFieldFocused ? "" : "Explore")
+        }
+    }
+}
+
+struct RecentSearchesView: View {
+    @FocusState.Binding var isSearchFieldFocused: Bool
+    @Binding var searchText: String
+    let recentSearches = ["Ha", "UIKit", "iOS 17", "Xcode", "Instagram Clone"]
+
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text("Recent Searches")
+                .font(.headline)
+                .padding(.leading)
+            
+            List(recentSearches, id: \.self) { search in
+                Button(action: {
+                    searchText = search
+                    isSearchFieldFocused = false
+                }) {
+                    Text(search)
+                        .foregroundColor(.black)
+                        .padding(.vertical, 5)
+                }
+            }
+            .listStyle(PlainListStyle())
         }
     }
 }
