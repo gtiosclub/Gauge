0271190d62a36561ae480a77d3076b7a63f624de
anthonyle51 <anthonyle20045@gmail.com>
2025-03-03 22:47:31 -0500

    categories

--- Gauge/Profiles+Search/Views/SearchView.swift
+++ Gauge/Profiles+Search/Views/SearchView.swift
@@ -9,6 +9,13 @@ import SwiftUI
 
 struct SearchView: View {
     @State private var searchText: String = ""
+    let items = Array(1...50).map { "Category \($0)" }
+    
+    // Define the grid columns
+    let columns = [
+        GridItem(.flexible()),
+        GridItem(.flexible())
+    ]
     var body: some View {
         NavigationView {
             VStack {
@@ -27,12 +34,47 @@ struct SearchView: View {
                     .cornerRadius(12)
                 }
                 .padding(.horizontal)
-                .padding(.bottom)
+                .padding(.bottom, 20)
                 .background(Color(red: 0.4627, green: 0.8392, blue: 1.0))
-                Spacer()
                 
+                // Categories
+                Text("Categories")
+                    .font(.headline)
+                    .bold()
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding(.horizontal)
                 
-                
+                ScrollView {
+                    LazyVGrid(columns: columns, spacing: 10) {
+                        ForEach(items.indices, id: \.self) { index in
+                            if index < 2 {
+                                Section {
+                                    
+                                } header: {
+                                    ZStack {
+                                        RoundedRectangle(cornerRadius: 10)
+                                            .fill(Color.red)
+                                            .frame(height: 100)
+                                        Text(items[index])
+                                            .foregroundColor(.white)
+                                            .font(.headline)
+                                    }
+                                }
+                            } else {
+                                ZStack {
+                                    RoundedRectangle(cornerRadius: 10)
+                                        .fill(Color.blue)
+                                        .frame(height: 100)
+                                    
+                                    Text(items[index])
+                                        .foregroundColor(.white)
+                                        .font(.headline)
+                                }
+                            }
+                        }
+                    }
+                }
+                .padding(.horizontal)
                 
                 
                 
