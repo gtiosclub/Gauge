d995d5db7c0c7d4e1f6a3d9a27674b6a8bd5df5f
DattaKansal <dtkansal@gmail.com>
2025-03-06 01:24:14 -0500

    added profile photo functionality

--- Gauge/Profiles+Search/ViewModels/ProfileViewModel.swift
+++ Gauge/Profiles+Search/ViewModels/ProfileViewModel.swift
@@ -14,204 +14,109 @@ class ProfileViewModel: ObservableObject {
     
     @Published var user: User?
     
-    private var db = Firestore.firestore()
     private var storage = Storage.storage()
     
     func storeImageAndReturnURL(userId: String, image: UIImage) async -> String? {
-        
-        // firebase storage accepts image in data format
         guard let imageData = image.jpegData(compressionQuality: 0.75) else {
-            print("couldn't convert image to data")
+            print("Couldn't convert image to data")
             return nil
         }
         
-        // reference to file in storage
-        let storageRef = storage.reference().child("profilePictures/\(userId).jpg")
+        let storageRef = storage.reference().child("images/\(userId).jpg")
         
         do {
-            // upload image to storage
             _ = try await storageRef.putDataAsync(imageData)
             
-            // getting the url
             let url = try await storageRef.downloadURL()
             return url.absoluteString
         } catch {
-            print("couldn't upload image: \(error.localizedDescription)")
+            print("Couldn't upload image: \(error.localizedDescription)")
             return nil
         }
-        
     }
     
     func getImageFromURL(from urlString: String) async -> UIImage? {
-        
         guard let url = URL(string: urlString) else {
-            print("the URL is invalid.")
+            print("The URL is invalid.")
             return nil
         }
         
         do {
-            // fetch image from URL
             let (data, _) = try await URLSession.shared.data(from: url)
             
-            // convert data to image
             if let image = UIImage(data: data) {
                 return image
             } else {
-                print("couldn't convert data to image.")
+                print("Couldn't convert data to image.")
                 return nil
             }
         } catch {
-            print("couldn't fetch image from URL: \(error.localizedDescription)")
+            print("Couldn't fetch image from URL: \(error.localizedDescription)")
             return nil
         }
-        
     }
     
     func removeAndReturn(userId: String) async -> Bool {
-        
-        let storageRef = storage.reference().child("profilePictures/\(userId).jpg")
+        let storageRef = storage.reference().child("images/\(userId).jpg")
         
         do {
-            // delete image from storage
             try await storageRef.delete()
-            print("profile picture deleted.")
+            print("Profile picture deleted.")
             return true
         } catch {
-            print("couldn't delete profile picture: \(error.localizedDescription)")
+            print("Couldn't delete profile picture: \(error.localizedDescription)")
+            return false
+        }
+    }
+    
+    func updateProfilePicture(userID: String, image: UIImage) async {
+        guard let profilePhotoURL = await storeImageAndReturnURL(userId: userID, image: image) else {
+            print("Failed to get download URL")
+            return
+        }
+        
+        let userDocument = Firebase.db.collection("USERS").document(userID)
+        do {
+            try await userDocument.updateData(["profilePhoto": profilePhotoURL])
+            print("Document successfully updated")
+        } catch {
+            print("Error updating document: \(error.localizedDescription)")
+        }
+    }
+    
+    func removeProfilePicture(userID: String) async -> Bool {
+        let userDocument = Firebase.db.collection("USERS").document(userID)
+        
+        do {
+            try await userDocument.updateData(["profilePhoto": FieldValue.delete()])
+            print("Profile picture field removed from Firestore")
+        } catch {
+            print("Error updating profile picture field: \(error.localizedDescription)")
             return false
         }
         
+        let removed = await removeAndReturn(userId: userID)
+        if !removed {
+            print("Failed to successfully remove profile picture from Storage")
+        }
+        
+        return removed
     }
     
-    //store the image URL that is provided by FirebaseStorage
-    //add/update profile picture
-    //    func updateProfilePicture(userID: String, image: UIImage) async -> Void {
-    //        //update profilePhoto field
-    //        guard let profilePhoto = storeImageAndReturnURL(image: image) else {
-    //            print("Failed to get download URL")
-    //            return
-    //        }
-    //        //update profile picture in firestore
-    //        let userDocument = Firebase.db.collection("USERS").document(userID)
-    //        userDocument.updateData(["profilePhoto": profilePhoto]) { error in
-    //            if let error = error {
-    //                print("Error updating document: \(error.localizedDescription)")
-    //            } else {
-    //                print("Document successfully updated")
-    //            }
-    //        }
-    //
-    //    }
-    //
-    
-    //remove profile picture
-    //    func removeProfilePicture(userID: String) async -> Bool {
-    //
-    //        //update profile picture in firestore
-    //        let userDocument = Firebase.db.collection("USERS").document(userID)
-    //
-    //        do {
-    //            //delete data in database
-    //            try await userDocument.updateData(["profilePhoto": FieldValue.delete()])
-    //            print("Profile Picture updated!")
-    //        } catch {
-    //            print("Error updating Profile Picture: \(error.localizedDescription)")
-    //            return false
-    //        }
-    //
-    //        //removeAndReturn returns true if image successfully removed from firebaseStorage
-    //        let removed = await removeAndReturn(userID)
-    //        if !removed {
-    //            print("failed to successfully remove profile picture")
-    //        }
-    //
-    //        return removed
-    //    }
-    
-    
-    
-    //get pfp function
-    //    func getProfilePicture(userID: String) async -> UIImage {
-    //        //user
-    //        let userDocument = Firebase.db.collection("USERS").document(userID)
-    //
-    //        do {
-    //            let document = try await userDocument.getDocument()
-    //            if let data = document.data(),
-    //               let urlString = data["profilePicture"] as? String {
-    //                return await getImageFromURL(urlString: urlString)
-    //            } else {
-    //                print("Document does not exist or URL could not be retrieved")
-    //            }
-    //        } catch {
-    //            print("Error fetching document: \(error.localizedDescription)")
-    //        }
-    //
-    //    }
-    
-    //store the image URL that is provided by FirebaseStorage
-    //add/update profile picture
-    //    func updateProfilePicture(userID: String, image: UIImage) async -> Void {
-    //        //update profilePhoto field
-    //        guard let profilePhoto = storeImageAndReturnURL(image: image) else {
-    //            print("Failed to get download URL")
-    //            return
-    //        }
-    //        //update profile picture in firestore
-    //        let userDocument = Firebase.db.collection("USERS").document(userID)
-    //        userDocument.updateData(["profilePhoto": profilePhoto]) { error in
-    //            if let error = error {
-    //                print("Error updating document: \(error.localizedDescription)")
-    //            } else {
-    //                print("Document successfully updated")
-    //            }
-    //        }
-    //
-    //    }
-    //
-    
-    //remove profile picture
-    //    func removeProfilePicture(userID: String) async -> Bool {
-    //
-    //        //update profile picture in firestore
-    //        let userDocument = Firebase.db.collection("USERS").document(userID)
-    //
-    //        do {
-    //            //delete data in database
-    //            try await userDocument.updateData(["profilePhoto": FieldValue.delete()])
-    //            print("Profile Picture updated!")
-    //        } catch {
-    //            print("Error updating Profile Picture: \(error.localizedDescription)")
-    //            return false
-    //        }
-    //
-    //        //removeAndReturn returns true if image successfully removed from firebaseStorage
-    //        let removed = await removeAndReturn(userID)
-    //        if !removed {
-    //            print("failed to successfully remove profile picture")
-    //        }
-    //
-    //        return removed
-    //    }
-    
-    
-    
-    //get pfp function
-    //    func getProfilePicture(userID: String) async -> UIImage {
-    //        //user
-    //        let userDocument = Firebase.db.collection("USERS").document(userID)
-    //
-    //        do {
-    //            let document = try await userDocument.getDocument()
-    //            if let data = document.data(),
-    //               let urlString = data["profilePicture"] as? String {
-    //                return await getImageFromURL(urlString: urlString)
-    //            } else {
-    //                print("Document does not exist or URL could not be retrieved")
-    //            }
-    //        } catch {
-    //            print("Error fetching document: \(error.localizedDescription)")
-    //        }
-    //
-    //    }
+
+    func getProfilePicture(userID: String) async -> UIImage? {
+        let userDocument = Firebase.db.collection("USERS").document(userID)
+        
+        do {
+            let document = try await userDocument.getDocument()
+            if let data = document.data(), let urlString = data["profilePhoto"] as? String {
+                return await getImageFromURL(from: urlString)
+            } else {
+                print("Document does not exist or URL could not be retrieved")
+            }
+        } catch {
+            print("Error fetching document: \(error.localizedDescription)")
+        }
+        return nil
+    }
 }
--- Gauge/Profiles+Search/Views/ProfileEditView.swift
+++ Gauge/Profiles+Search/Views/ProfileEditView.swift
@@ -1,115 +1,172 @@
-//
-//  ProfileEditView.swift
-//  Gauge
-//
-//  Created by Sam Orouji on 2/25/25.
-//
+////
+////  ProfileEditView.swift
+////  Gauge
+////
+////  Created by Sam Orouji on 2/25/25.
+////
+////
 
 import SwiftUI
 
 struct ProfileEditView: View {
+    @StateObject var profileViewModel = ProfileViewModel()
+    let userID: String
+    
+    // Local states
     @State private var username: String = ""
+    @State private var showingPhotoOptions = false
+    @State private var showingImagePicker = false
+    @State private var selectedImage: UIImage?
+    @State private var profileImage: UIImage?
     
     var body: some View {
         VStack {
+            // Navigation Bar Section
             Section {
                 HStack {
-                    NavigationLink(destination: ProfileEditView()) {
-                        Button("Cancel") {}
+                    Button("Cancel") {
+                        // Dismiss or pop the view
                     }
-                    
-                    Spacer() // Pushes "Save" button to the right
-                    Button("Save") {}
+                    Spacer()
+                    Button("Save") {
+                        Task {
+                            if profileImage == nil {
+                                let _ = await profileViewModel.removeProfilePicture(userID: userID)
+                            } else if let newImage = selectedImage {
+                                await profileViewModel.updateProfilePicture(userID: userID, image: newImage)
+                            }
+                            // add additional updates for username or other fields here
+                        }
+                    }
                 }
-                .padding(.horizontal) // Adds even horizontal padding
+                .padding(.horizontal)
             }
             
-            Circle()
-                .frame(width: 150, height: 150)
-                .foregroundColor(Color.gray)
+            ZStack {
+                if let profileImage = profileImage {
+                    Image(uiImage: profileImage)
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .frame(width: 150, height: 150)
+                        .clipShape(Circle())
+                } else {
+                    Circle()
+                        .fill(Color.gray)
+                        .frame(width: 150, height: 150)
+                }
+            }
+            .overlay(
+                Button {
+                    showingPhotoOptions = true
+                } label: {
+                    ZStack {
+                        Circle()
+                            .fill(Color.black)
+                            .frame(width: 30, height: 30)
+                        
+                        Image(systemName: "pencil")
+                            .foregroundColor(.white)
+                            .font(.system(size: 14, weight: .bold))
+                    }
+                }
+                .padding(8),
+                alignment: .bottomTrailing
+            )
+            .confirmationDialog(
+                "Do you want to change your profile picture?",
+                isPresented: $showingPhotoOptions,
+                titleVisibility: .visible
+            ) {
+                Button("Choose from library") {
+                    showingImagePicker = true
+                }
+                Button("Remove current picture", role: .destructive) {
+                    profileImage = nil
+                    selectedImage = nil
+                }
+                Button("Cancel", role: .cancel) {}
+            }
+            .sheet(isPresented: $showingImagePicker) {
+                ImagePicker(selectedImage: $selectedImage, sourceType: .photoLibrary)
+            }
+            .onChange(of: selectedImage) { newImage in
+                if let newImage = newImage {
+                    profileImage = newImage
+                }
+            }
             
-            Spacer()
-                .frame(height: 50)
+            Spacer().frame(height: 50)
             
             VStack(alignment: .leading, spacing: 10) {
+                // Username
                 HStack(spacing: 20) {
                     Text("Username")
-                        .frame(width: 80, alignment: .leading) // Ensures text aligns
+                        .frame(width: 80, alignment: .leading)
                     TextField("new username", text: $username)
-                        .textFieldStyle(PlainTextFieldStyle()) // Remove text box
-                        .foregroundColor(.primary)
-                }
-                .padding(.leading, 20) // Consistent left padding
-                .padding(.bottom, 5)
-                .padding(.top, 5)
-                
-                Divider()
-                
-                HStack(spacing: 20) {
-                    Text("Pronouns")
-                        .frame(width: 80, alignment: .leading)
-                    TextField("Pronouns", text: $username)
                         .textFieldStyle(PlainTextFieldStyle())
                         .foregroundColor(.primary)
                 }
                 .padding(.leading, 20)
-                .padding(.bottom, 5)
-                .padding(.top, 5)
-                
+                .padding(.vertical, 5)
                 Divider()
-                
-                HStack(spacing: 20) {
-                    Text("Bio")
-                        .frame(width: 80, alignment: .leading)
-                    TextField("a short bio that describes the user", text: $username)
-                        .textFieldStyle(PlainTextFieldStyle())
-                        .foregroundColor(.primary)
-                }
-                .padding(.leading, 20)
-                .padding(.bottom, 5)
-                .padding(.top, 5)
-
-                
-                Divider()
-                
-                HStack {
-                    Text("User Tags")
-                    Spacer()
-                    NavigationLink(destination: ProfileEditView()) {
-                        HStack {
-                            Text("4")
-                            Image(systemName: "chevron.right")
-                        }
-                    }
-                }
-                .padding(.horizontal, 20) // Align with previous elements
-                .padding(.bottom, 5)
-                .padding(.top, 5)
-
-                
-                Divider()
-                
-                HStack {
-                    Text("Badges")
-                    Spacer()
-                    NavigationLink(destination: ProfileEditView()) {
-                        HStack {
-                            Text("5")
-                            Image(systemName: "chevron.right")
-                        }
-                    }
-                }
-                .padding(.horizontal, 20)
-                .padding(.top, 5)
-                .padding(.bottom , 5)
-                
+                // Pronouns
+               HStack(spacing: 20) {
+                   Text("Pronouns")
+                       .frame(width: 80, alignment: .leading)
+                   TextField("Pronouns", text: $username)
+                       .textFieldStyle(PlainTextFieldStyle())
+                       .foregroundColor(.primary)
+               }
+               .padding(.leading, 20)
+               .padding(.vertical, 5)
+               Divider()
+                               
+               // Bio
+               HStack(spacing: 20) {
+                   Text("Bio")
+                       .frame(width: 80, alignment: .leading)
+                   TextField("a short bio that describes the user", text: $username)
+                       .textFieldStyle(PlainTextFieldStyle())
+                       .foregroundColor(.primary)
+               }
+               .padding(.leading, 20)
+               .padding(.vertical, 5)
+               Divider()
+                               
+               // User Tags
+               HStack {
+                   Text("User Tags")
+                   Spacer()
+                   NavigationLink(destination: Text("User Tags Screen")) {
+                       HStack {
+                           Text("4")
+                           Image(systemName: "chevron.right")
+                       }
+                   }
+               }
+               .padding(.horizontal, 20)
+               .padding(.vertical, 5)
+               Divider()
+                               
+               // Badges
+               HStack {
+                   Text("Badges")
+                   Spacer()
+                   NavigationLink(destination: Text("Badges Screen")) {
+                       HStack {
+                           Text("5")
+                           Image(systemName: "chevron.right")
+                       }
+                   }
+               }
+               .padding(.horizontal, 20)
+               .padding(.vertical, 5)
             }
-            .frame(maxWidth: .infinity, alignment: .leading) // Ensures everything aligns to the left
+            .frame(maxWidth: .infinity, alignment: .leading)
             
             Spacer()
             
             Divider()
-            
             HStack {
                 VStack {
                     Image(systemName: "star.fill")
@@ -119,9 +176,7 @@ struct ProfileEditView: View {
                         .foregroundColor(.gray)
                         .font(.system(size: 12))
                 }
-                
                 Spacer()
-                
                 VStack {
                     Image(systemName: "star.fill")
                         .foregroundColor(.gray)
@@ -130,28 +185,67 @@ struct ProfileEditView: View {
                         .foregroundColor(.gray)
                         .font(.system(size: 12))
                 }
-                
                 Spacer()
-                
                 VStack {
                     Image(systemName: "person.fill")
                         .foregroundColor(.blue)
                         .font(.system(size: 24))
                     Text("Profile")
-                        .multilineTextAlignment(.center)
                         .foregroundColor(.blue)
                         .font(.system(size: 12))
                 }
             }
-            .padding(.horizontal, 40) // Ensures even padding for tab bar items
+            .padding(.horizontal, 40)
+        }
+        .task {
+            profileImage = await profileViewModel.getProfilePicture(userID: userID)
         }
     }
 }
 
+#Preview {
+    ProfileEditView(userID: "xEZWt93AaJZPwfHAjlqMjmVP0Lz1")
+}
 
+
+
+struct ImagePicker: UIViewControllerRepresentable {
+    @Environment(\.presentationMode) var presentationMode
+    @Binding var selectedImage: UIImage?
+    let sourceType: UIImagePickerController.SourceType
     
-    
-    
-    #Preview {
-        ProfileEditView()
+    func makeUIViewController(context: Context) -> UIImagePickerController {
+        let picker = UIImagePickerController()
+        picker.delegate = context.coordinator
+        picker.sourceType = sourceType
+        return picker
     }
+    
+    func updateUIViewController(_ uiViewController: UIImagePickerController, context: Context) {}
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+    
+    class Coordinator: NSObject, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+        let parent: ImagePicker
+        
+        init(_ parent: ImagePicker) {
+            self.parent = parent
+        }
+        
+        func imagePickerController(
+            _ picker: UIImagePickerController,
+            didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]
+        ) {
+            if let uiImage = info[.originalImage] as? UIImage {
+                parent.selectedImage = uiImage
+            }
+            parent.presentationMode.wrappedValue.dismiss()
+        }
+        
+        func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+            parent.presentationMode.wrappedValue.dismiss()
+        }
+    }
+}
