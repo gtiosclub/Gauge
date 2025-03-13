c6cc534b36b38b57834e069767455a2dcab21a47
kavya-adusu <kavyaadusumi@gmail.com>
2025-03-13 17:18:46 -0400

    date converter

--- Gauge/Feed/ViewModels/PostFirebase.swift
+++ Gauge/Feed/ViewModels/PostFirebase.swift
@@ -472,7 +472,9 @@ class PostFirebase: ObservableObject {
                             let post = BinaryPost(postId: newPostData["postId"] as? String ?? "",
                                                   userId: newPostData["userId"] as? String ?? "",
                                                   categories: newPostData["categories"] as? [Category] ?? [],
-                                                  postDateAndTime: DateConverter.convertStringToDate(newPostData["postDateAndTime"] as? String ?? "") ?? Date(),
+                                                  postDateAndTime: (newPostData["postDateAndTime"] as? Timestamp)?.dateValue()
+                                                      ?? DateConverter.convertStringToDate(newPostData["postDateAndTime"] as? String ?? "")
+                                                      ?? Date(),
                                                   question: newPostData["question"] as? String ?? "",
                                                   responseOption1: newPostData["responseOption1"] as? String ?? "",
                                                   responseOption2: newPostData["responseOption2"] as? String ?? "",
@@ -484,7 +486,9 @@ class PostFirebase: ObservableObject {
                             let post = SliderPost(postId: newPostData["postId"] as? String ?? "",
                                                   userId: newPostData["userId"] as? String ?? "",
                                                   categories: newPostData["categories"] as? [Category] ?? [],
-                                                  postDateAndTime: DateConverter.convertStringToDate(newPostData["postDateAndTime"] as? String ?? "") ?? Date(),
+                                                  postDateAndTime: (newPostData["postDateAndTime"] as? Timestamp)?.dateValue()
+                                                      ?? DateConverter.convertStringToDate(newPostData["postDateAndTime"] as? String ?? "")
+                                                      ?? Date(),
                                                   question: newPostData["question"] as? String ?? "",
                                                   lowerBoundValue: newPostData["lowerBoundValue"] as? Double ?? 0,
                                                   upperBoundValue: newPostData["upperBoundValue"] as? Double ?? 1,
@@ -498,7 +502,9 @@ class PostFirebase: ObservableObject {
                             let post = RankPost(postId: newPostData["postId"] as? String ?? "",
                                                   userId: newPostData["userId"] as? String ?? "",
                                                   categories: newPostData["categories"] as? [Category] ?? [],
-                                                  postDateAndTime: DateConverter.convertStringToDate(newPostData["postDateAndTime"] as? String ?? "") ?? Date(),
+                                                postDateAndTime: (newPostData["postDateAndTime"] as? Timestamp)?.dateValue()
+                                                    ?? DateConverter.convertStringToDate(newPostData["postDateAndTime"] as? String ?? "")
+                                                    ?? Date(),
                                                   question: newPostData["question"] as? String ?? "",
                                                   responseOptions: newPostData["responseOptions"] as? [String] ?? [],
                                                   favoritedBy: newPostData["favoritedBy"] as? [String] ?? [])
