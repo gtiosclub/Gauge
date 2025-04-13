a6bb69cbd465d148c1b53df9e2e1eec45ae6430d
Akshat-Shenoi <ashenoi3@gatech.edu>
2025-04-13 15:05:51 -0400

    host won't get take master now

--- Gauge/Games/TakeMaster/MPC/TMManager.swift
+++ Gauge/Games/TakeMaster/MPC/TMManager.swift
@@ -342,6 +342,9 @@ extension TMManager: MCSessionDelegate {
 
                     if self.questionSubmissions.count >= self.expectedPlayers {
                         self.currentQuestion = self.questionSubmissions.randomElement()
+                        let hostID = self.discoveredPeers.first(where: { $0.value.isHost == "Y" })?.key.displayName
+                        let nonHostSubmissions = self.questionSubmissions.filter { $0.playerID != hostID }
+                        self.currentQuestion = nonHostSubmissions.randomElement()
                         self.phase = .roundStart
                         self.broadcastRoundStart(self.currentQuestion!)
                     } else {
